2870c6fc6d338870816294cdee878e75c4336973
ajazzbegins <34200494+ajazzbegins@users.noreply.github.com>
2022-12-26 15:06:01 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("ajaaz");
+		System.out.println("ajaaz -- this is Jenkins training tutorial");
 
 	}
 
